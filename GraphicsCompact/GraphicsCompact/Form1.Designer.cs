57ff7cf110b4632a8b7ce25c53b6117f6c0a28dd
YonatanGideoni <yonatan.gideoni@gmail.com>
2017-02-21 00:05:07 +0200

    Graphics Compacting Program
    C# program which compacts the graphics so it can be easily and
    efficiently used in assembly.

---
+++ GraphicsCompact/GraphicsCompact/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace GraphicsCompact
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CreateBoard = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BoardSize = new System.Windows.Forms.NumericUpDown();
+            this.PrintButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.BoardSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CreateBoard
+            // 
+            this.CreateBoard.Location = new System.Drawing.Point(13, 13);
+            this.CreateBoard.Name = "CreateBoard";
+            this.CreateBoard.Size = new System.Drawing.Size(95, 23);
+            this.CreateBoard.TabIndex = 0;
+            this.CreateBoard.Text = "Create Panel";
+            this.CreateBoard.UseVisualStyleBackColor = true;
+            this.CreateBoard.Click += new System.EventHandler(this.CreateBoard_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(13, 43);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 1;
+            // 
+            // BoardSize
+            // 
+            this.BoardSize.Location = new System.Drawing.Point(134, 13);
+            this.BoardSize.Name = "BoardSize";
+            this.BoardSize.Size = new System.Drawing.Size(41, 20);
+            this.BoardSize.TabIndex = 2;
+            this.BoardSize.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // PrintButton
+            // 
+            this.PrintButton.Location = new System.Drawing.Point(197, 14);
+            this.PrintButton.Name = "PrintButton";
+            this.PrintButton.Size = new System.Drawing.Size(75, 23);
+            this.PrintButton.TabIndex = 3;
+            this.PrintButton.Text = "Compress!";
+            this.PrintButton.UseVisualStyleBackColor = true;
+            this.PrintButton.Click += new System.EventHandler(this.PrintButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.PrintButton);
+            this.Controls.Add(this.BoardSize);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.CreateBoard);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.BoardSize)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreateBoard;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown BoardSize;
+        private System.Windows.Forms.Button PrintButton;
+    }
+}
+
---
+++ GraphicsCompact/GraphicsCompact/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsCompact
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        short[,] intBoard;
+        Button[,] Board;
+
+        private void CreateBoard_Click(object sender, EventArgs e)
+        {
+            short BoardLength = (short)(BoardSize.Value);
+            Board = new Button[BoardLength, BoardLength];
+            intBoard = new short[BoardLength, BoardLength];
+
+            short ButtonSize = 10;
+
+            panel1.Size = new Size(ButtonSize * BoardLength, ButtonSize * BoardLength);
+            this.Size = new Size(panel1.Width + 50, panel1.Height + 100);
+
+            for (short i = 0; i < BoardLength; i++)
+            {
+                for (short k = 0; k < BoardLength; k++)
+                {
+                    Board[i, k] = new Button();
+                    Board[i, k].Size = new Size(ButtonSize, ButtonSize);
+                    Board[i, k].Location = new Point(ButtonSize * i, ButtonSize * k);
+                    Board[i, k].Tag = new short[2] { i, k };
+
+                    if (i % 2 == 0)
+                    {
+                        intBoard[i, k] = 0;
+                        Board[i, k].BackColor = Color.White;
+                    }
+                    else
+                    {
+                        intBoard[i, k] = 1;
+                        Board[i, k].BackColor = Color.Black;
+                    }
+                    
+
+                    panel1.Controls.Add(Board[i, k]);
+                    Board[i, k].Click += Form1_Click;
+                }
+            }
+        }
+
+        void Form1_Click(object sender, EventArgs e)
+        {
+            short[] ButtonLoc = (short[])((Array)(((Button)(sender)).Tag));
+            short row = ButtonLoc[0];
+            short col = ButtonLoc[1];
+
+            if (intBoard[row, col] == 1)
+            {
+                Board[row, col].BackColor = Color.White;
+                intBoard[row, col] = 0;
+            }
+            else
+            {
+                Board[row, col].BackColor = Color.Black;
+                intBoard[row, col] = 1;
+            }
+        }
+
+        private void PrintButton_Click(object sender, EventArgs e)
+        {
+            short BoardLength = (short)(BoardSize.Value);
+            string[] compressedGraphic = new string[BoardLength * BoardLength];
+            short whiteIndexes = 0;
+            short blackIndexes = 0;
+            short indexType;
+            short stringIndex = 0;
+
+            for (short i = 0; i < BoardLength; i++)
+            {
+                indexType = intBoard[i, 0];
+                for (short k = 0; k < BoardLength; k++)
+                {
+                    if (intBoard[i, k] != indexType)
+                    {
+                        if (indexType == 0)
+                        {
+                            compressedGraphic[stringIndex] = whiteIndexes.ToString() + ",";
+                        }
+                        else
+                        {
+                            compressedGraphic[stringIndex] = blackIndexes.ToString() + ",";
+                        }
+                        indexType = (short)((indexType + 1) % 2);
+                        stringIndex++;
+                        whiteIndexes = 0;
+                        blackIndexes = 0;
+                    }
+                    if (indexType == 0)
+                    {
+                        whiteIndexes++;
+                    }
+                    else
+                    {
+                        blackIndexes++;
+                    }
+                }
+                if (indexType == 0)
+                {
+                    compressedGraphic[stringIndex] = whiteIndexes.ToString() + ",";
+                }
+                else
+                {
+                    compressedGraphic[stringIndex] = blackIndexes.ToString() + ",";
+                }
+                stringIndex++;
+                blackIndexes = 0;
+                whiteIndexes = 0;
+                compressedGraphic[stringIndex] = "255,";
+                stringIndex++;
+            }
+            compressedGraphic[stringIndex] = "254";
+
+            string printedString = "";
+            for (short i = 0; i < compressedGraphic.Length; i++)
+            {
+                printedString += compressedGraphic[i];
+            }
+            System.Windows.Forms.Clipboard.SetDataObject(printedString, true);
+            MessageBox.Show(printedString);
+        }
+    }
+}
